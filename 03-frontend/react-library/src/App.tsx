cfc3f92c60a8ae15f533870c8413b9862aff8b16
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-09-26 01:59:02 +0530

    home page completed

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
