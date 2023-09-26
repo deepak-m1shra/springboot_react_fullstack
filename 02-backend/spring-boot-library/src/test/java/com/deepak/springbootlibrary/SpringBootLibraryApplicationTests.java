24c77078d10b1b5918ee718251cdf2a74107d1d3
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-09-26 16:01:53 +0530

    added backend folder

---
+++ 02-backend/spring-boot-library/src/test/java/com/deepak/springbootlibrary/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.springbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
